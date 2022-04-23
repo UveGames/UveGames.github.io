200e8f2e95a6a5e6b4f20718e523f0aa660846e3
Uve <100414155+UveGames@users.noreply.github.com>
2022-04-23 21:17:21 +0200

    Update login.js

--- login.js
+++ login.js
@@ -3,8 +3,8 @@
 function login () {
 baseDeDatos ={
 var user, pass;
-  user = document.getElementById("usuario").value;
-  pass = document.getElementById("contraseña").value;
+  user = document.getElementById("Usuario").value;
+  pass = document.getElementById("Contraseña").value;
 
 if(user == "Dios" && pass == "Dios"){src"youtube.com"
   
