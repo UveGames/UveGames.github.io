42a22115ca45d0b94e86c06f2f7639f5d78ce40d
Uve <100414155+UveGames@users.noreply.github.com>
2022-04-24 17:05:48 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,12 @@
 /*datos de prueba y Elementos por defecto*/
 
 function login() {
-var c="Dios";
-var u="Dios";
-  
 var user=
     document.getElementById("login")
 var pass=
     document.getElementById("password")
 
-if(document.form.password.value===c && document.form.login.value===u)
+if(login.value === "Dios" && password.value === "Dios")
   window.location="Youtube.com";
   else
   alert("Usuario o Contraseña Incorectos");
